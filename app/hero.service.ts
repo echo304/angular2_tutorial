c8787efcc109d78b828ab311e1f60dd72ff74d91
S.B. Lee <echo3042@gmail.com>
2016-05-22 13:38:34 -0700

    Add Router

---
+++ app/hero.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+  getHeroes() {
+    return Promise.resolve(HEROES);
+  }
+  getHeroesSlowly() {
+    return new Promise<Hero[]>(resolve => {
+      return setTimeout(() => resolve(HEROES), 2000);
+    });
+  }
+}
